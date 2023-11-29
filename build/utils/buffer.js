c9241af37eb315e71bb877b128c84f86a2ef1187
kunroku <es-shirahama+shirahama@eversystem.co.jp>
2023-11-29 16:02:48 +0900

    [update] add buffer

--- build/utils/buffer.js
+++ build/utils/buffer.js
@@ -2,8 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Buffer = void 0;
 const buffer_1 = require("buffer");
-if (!window.Buffer) {
-    window.Buffer = buffer_1.Buffer;
-}
 exports.Buffer = buffer_1.Buffer;
 //# sourceMappingURL=buffer.js.map
--- src/utils/buffer.ts
+++ src/utils/buffer.ts
@@ -1,7 +1,3 @@
 import { Buffer as NodeJSBuffer } from 'buffer';
 
-if (!window.Buffer) {
-  window.Buffer = NodeJSBuffer;
-}
-
 export const Buffer = NodeJSBuffer;
