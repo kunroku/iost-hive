db1b1dfc38a868b9124bbbc5ef69a482c9da7df9
kunroku <es-shirahama+shirahama@eversystem.co.jp>
2023-11-29 16:49:26 +0900

    [update] add buffer

--- build/utils/sha3.d.ts
+++ build/utils/sha3.d.ts
@@ -1,2 +1,2 @@
-export type SHA3_HASH_SIZE = 512 | 224 | 256 | 384;
+export type SHA3_HASH_SIZE = 512 | 384 | 256 | 224;
 export declare const sha3: (size: SHA3_HASH_SIZE, buf: Buffer) => Buffer;
--- build/utils/sha3.js
+++ build/utils/sha3.js
@@ -1,11 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sha3 = void 0;
-const sha3_1 = require("sha3");
+const js_sha3_1 = require("js-sha3");
+const getHashFunc = (size) => {
+    switch (size) {
+        case 512:
+            return js_sha3_1.sha3_512;
+        case 384:
+            return js_sha3_1.sha3_384;
+        case 256:
+            return js_sha3_1.sha3_256;
+        case 224:
+            return js_sha3_1.sha3_224;
+        default:
+            break;
+    }
+};
 const sha3 = (size, buf) => {
-    const hash = new sha3_1.SHA3(size);
-    hash.update(buf);
-    return hash.digest('binary');
+    return Buffer.from(getHashFunc(size)(buf), 'hex');
 };
 exports.sha3 = sha3;
 //# sourceMappingURL=sha3.js.map
